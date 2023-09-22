217999e55057b4638ba9fc8f9e59585721485896
s-light <git@s-light.eu>
2023-09-22 22:26:33 +0200

    add build documentation

--- CIRCUITPY_disc/config.py
+++ CIRCUITPY_disc/config.py
@@ -18,6 +18,6 @@ config = {
         # "mode": "nightlight",
         "brightness": 0.02,
         # https://learn.adafruit.com/fancyled-library-for-circuitpython/colors#hsv-colors-2981215
-        # "base_color": CHSV(0.7),  # only specifiing Hue. purple
+        "base_color": CHSV(0.7),  # only specifiing Hue. purple
     },
 }
