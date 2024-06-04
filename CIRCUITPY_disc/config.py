d7d1d5fe14696ac1cc2c48794a868291ea450873
s-light <git@s-light.eu>
2024-06-04 17:51:49 +0200

    extend config

--- CIRCUITPY_disc/config.py
+++ CIRCUITPY_disc/config.py
@@ -4,16 +4,31 @@ from adafruit_fancyled.adafruit_fancyled import CHSV, CRGB
 
 config = {
     # in wich mode to start: (RGBLamp | POVPainter)
-    # "start_mode": "RGBLamp",
-    "start_mode": "POVPainter",
+    "start_mode": "RGBLamp",
+    # "start_mode": "POVPainter",
     "POVPainter": {
-        "brightness": 0.99  ,
+        "brightness": 0.99,
     },
+    # light painting
+    # "RGBLamp": {
+    #     "brightness": 1.0,
+    #     # effect duration in seconds
+    #     "effect_duration": 15,
+    #     "effect_active": True,
+    #     # purple
+    #     "color_range": {
+    #         "min": CHSV(0.50),
+    #         "max": CHSV(0.9),
+    #     },
+    #     "extra_effects": {
+    #         "y_to_brightness": False,
+    #     },
+    # },
     "RGBLamp": {
-        "brightness": 0.4,
+        "brightness": 0.5,
         # effect duration in seconds
-        "effect_duration": 10 * 60,
-        "effect_active": False,
+        "effect_duration": 3 * 60,
+        "effect_active": True,
         #
         # colors
         # https://learn.adafruit.com/fancyled-library-for-circuitpython/colors#hsv-colors-2981215
